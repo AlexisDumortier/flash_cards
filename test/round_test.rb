3ff0cf9c61818a80ba2291af48900fd79f4401cc
Alexis Dumortier <dumortier.alexis@gmail.com>
2019-11-21 20:18:52 -0700

    Add test round_has_a_current_card

--- test/round_test.rb
+++ test/round_test.rb
@@ -44,4 +44,17 @@ class RoundTest < Minitest::Test
     assert_equal [], round.turns
   end
 
+  def test_round_has_a_current_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5 clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_equal card_1, round.current_card
+
+  end
+
 end
