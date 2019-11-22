9a7994e3deaa76f05cde6430431c4eab01543260
Alexis Dumortier <dumortier.alexis@gmail.com>
2019-11-21 22:28:19 -0700

    Add a test of the output of the cards method of CardGenerator

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -10,4 +10,15 @@ class CardGeneratorTest < Minitest::Test
     assert_instance_of CardGenerator, new_set
   end
 
+  def test_output_of_cardgenerator_cards
+    # tests that .cards is a method of CardGenerator
+    assert_respond_to CardGenerator.new("cards.txt"), :cards
+    # tests that it returns an array
+    assert_kind_of Array, CardGenerator.new("cards.txt").cards
+    # tests that the elements of the array are cards
+    assert_kind_of Card, CardGenerator.new("cards.txt").cards.at(0)
+    assert_kind_of Card, CardGenerator.new("cards.txt").cards.at(1)
+    assert_kind_of Card, CardGenerator.new("cards.txt").cards.at(2)
+  end
+
 end
