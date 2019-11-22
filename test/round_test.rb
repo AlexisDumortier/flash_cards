34ff1c7234a66f6335aaecb5f94078fd35eabe65
Alexis Dumortier <dumortier.alexis@gmail.com>
2019-11-21 20:20:53 -0700

    Add test takes_turn_returns_the_correct_turn_and_changes_current_card

--- test/round_test.rb
+++ test/round_test.rb
@@ -54,7 +54,25 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
 
     assert_equal card_1, round.current_card
+  end
 
+  def test_takes_turn_returns_the_correct_turn_and_changes_current_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5 clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    turn1 = round.take_turn("Washington")
+    # tests that a turn is returned when the take_turn method is called
+    assert_instance_of Turn, turn1
+    # tests that the guess associated with the turn returne matches the test used when calling take_turn
+    assert_equal "Washington", turn1.guess
+    # tests that the turns array now has the turn initiated stored
+    assert_equal [turn1], round.turns
+    # test that after taking a turn the current card changes
+    assert_equal card_2, round.current_card
   end
 
 end
