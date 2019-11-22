1fae54984a4c91310db16fa4d7b390505669e69a
Alexis Dumortier <dumortier.alexis@gmail.com>
2019-11-21 22:31:37 -0700

    Change the runner file to use the cards generated from the textfile

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,14 +2,12 @@ require './lib/deck'
 require './lib/card'
 require './lib/turn'
 require './lib/round'
+require './lib/card_generator'
 
-card1 = Card.new("What's the capital of Japan?", "Tokyo", :Geography)
-card2 = Card.new("What is the firstname of Seinfeld's best friend?", "George", :Popculture)
-card3 = Card.new("Who is the oldest Beatles? (Ringo, John, Paul or George)", "Ringo", :Popculture)
-card4 = Card.new("How many states are there in the US?", "50", :Geography)
-card5 = Card.new("What is the square root of 169?", "13", :STEM)
+filename = "cards.txt"
 
-cards = [card1, card2, card3, card4, card5]
+cards = CardGenerator.new(filename).cards
+
 deck = Deck.new(cards)
 round = Round.new(deck)
 
